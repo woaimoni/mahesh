5e0d42f8203b0cf4599132356da8a0ecd1d3c481
Mahesh Yadav <maheshyadavkattaboyina@gmail.com>
2024-08-29 14:17:21 +0530

    commiting two files

---
+++ mahes.py
@@ -0,0 +1,8 @@
+# Function to print numbers from 1 to n
+def print_numbers(n):
+    for i in range(1, n + 1):
+        print(i)
+
+# Example usage
+n = int(input("Enter the value of n: "))  # Get user input
+print_numbers(n)
